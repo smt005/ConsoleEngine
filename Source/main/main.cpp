6fe65e4cbf3a412b59253740dc1af51900de020b
Коптяев Александр <a.koptyaev@playrix.com>
2022-03-10 14:11:21 +0300

    CMake для примера в шаблон

--- Source/main/main.cpp
+++ Source/main/main.cpp
@@ -1,16 +1,7 @@
 
-#define WINDOWS_HIDE_CONSOLE 1
-
-#if WINDOWS_HIDE_CONSOLE
-	#include <Windows.h>
-#endif
-
 #include "Execution.h"
 
 int main(int argc, char** argv)
 {
-#if WINDOWS_HIDE_CONSOLE
-	FreeConsole();
-#endif
 	return Execution::run();
 }
